f6909219e137445219d5fa651eb716758de69776
Andrey Petrov <19graff91@gmail.com>
2013-05-07 21:34:48 +0300

    Add menu item parser.

--- GrillMaster/GrillMaster.Services/Parsers/XmlParser.cs
+++ GrillMaster/GrillMaster.Services/Parsers/XmlParser.cs
@@ -25,12 +25,45 @@ namespace GrillMaster.Services.Parsers
         /// <returns>
         /// The <see cref="GrillMenu[]"/>.
         /// </returns>
-        public static List<GrillMenu> ParseMenu(XmlDocument xmlDocument)
+        public static List<GrillMenu> ParseGrillMenus(XmlDocument xmlDocument)
         {
             var menuesNode = xmlDocument.LastChild.ChildNodes.Cast<XmlNode>().Where(node => node.Name == "entry").ToList();
             var manager = CreateXmlManager(xmlDocument);
 
             return (from XmlNode node in menuesNode select ParseMenu(node, manager)).ToList();
+        }
+
+        public static List<GrillMenuItem> ParseGrillMenuItems(XmlDocument xmlDocument)
+        {
+            var menuItemsNodes = xmlDocument.LastChild.ChildNodes.Cast<XmlNode>().Where(node => node.Name == "entry").ToList();
+            var manager = CreateXmlManager(xmlDocument);
+
+            return (from XmlNode node in menuItemsNodes select ParseGrillMenuItem(node, manager)).ToList();
+        }
+
+        private static GrillMenuItem ParseGrillMenuItem(XmlNode menuNode, XmlNamespaceManager manager)
+        {
+            var menuName = string.Empty;
+            var guid = Guid.Empty;
+            var length = 0;
+            var width = 0;
+            var duration = TimeSpan.Zero;
+
+            foreach (XmlNode childNode in menuNode.ChildNodes)
+            {
+                switch (childNode.Name)
+                {
+                    case "content":
+                        menuName = childNode.SelectSingleNode("m:properties/d:Name", manager).InnerText;
+                        guid = XmlConvert.ToGuid(childNode.SelectSingleNode("m:properties/d:Id", manager).InnerText);
+                        length = Convert.ToInt32(childNode.SelectSingleNode("m:properties/d:Length", manager).InnerText);
+                        width = Convert.ToInt32(childNode.SelectSingleNode("m:properties/d:Width", manager).InnerText);
+                        duration = XmlConvert.ToTimeSpan(childNode.SelectSingleNode("m:properties/d:Duration", manager).InnerText);
+                        break;
+                }
+            }
+
+            return new GrillMenuItem(guid, menuName, length, width, duration);
         }
 
         /// <summary>
